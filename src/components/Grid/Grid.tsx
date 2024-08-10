73d5c39438bbebf4f85e629a791c07e7e5a673ee
chdonncha <chdonncha@gmail.com>
2024-08-10 20:20:30 +0100

    :recycle: Refactor extractor to excavator

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,13 +4,14 @@ import React, { useState } from 'react';
 import { GridSquare } from './GridSquare';
 
 import conveyorTexture from '../../textures/conveyor.png';
-import excavator1Texture from '../../textures/excavator.png';
-import excavator3Texture from '../../textures/excavator.png';
-import excavator2Texture from '../../textures/excavator.png';
-import assemblerMultispriteA from '../../textures/extractorA.png';
-import assemblerMultispriteB from '../../textures/extractorB.png';
-import assemblerMultispriteC from '../../textures/extractorC.png';
-import assemblerMultispriteD from '../../textures/extractorD.png';
+import assemblerMultispriteA from '../../textures/excavatorA.png';
+import excavatorMultispriteA from '../../textures/excavatorA.png';
+import excavatorMultispriteB from '../../textures/excavatorB.png';
+import assemblerMultispriteB from '../../textures/excavatorB.png';
+import excavatorMultispriteC from '../../textures/excavatorC.png';
+import assemblerMultispriteC from '../../textures/excavatorC.png';
+import excavatorMultispriteD from '../../textures/excavatorD.png';
+import assemblerMultispriteD from '../../textures/excavatorD.png';
 import { GridSprite } from '../CitySprite/GridSprite';
 import { GridOutline } from '../GridOutline/GridOutline';
 import './Grid.css';
@@ -195,9 +196,18 @@ const Grid: React.FC<GridProps> = ({ selectedBuilding, currentSelected, map }) =
       case 'assembler3C':
       case 'assembler3D':
         return 'lightgreen';
-      case 'excavator1':
-      case 'excavator2':
-      case 'excavator3':
+      case 'excavator1A':
+      case 'excavator1B':
+      case 'excavator1C':
+      case 'excavator1D':
+      case 'excavator2A':
+      case 'excavator2B':
+      case 'excavator2C':
+      case 'excavator2D':
+      case 'excavator3A':
+      case 'excavator3B':
+      case 'excavator3C':
+      case 'excavator3D':
         return 'yellow';
       case 'conveyor':
         return 'dimgrey';
@@ -234,12 +244,22 @@ const Grid: React.FC<GridProps> = ({ selectedBuilding, currentSelected, map }) =
       case 'assembler2D':
       case 'assembler3D':
         return assemblerMultispriteD;
-      case 'excavator1':
-        return excavator1Texture;
-      case 'excavator2':
-        return excavator2Texture;
-      case 'excavator3':
-        return excavator3Texture;
+      case 'excavator1A':
+      case 'excavator2A':
+      case 'excavator3A':
+        return excavatorMultispriteA;
+      case 'excavator1B':
+      case 'excavator2B':
+      case 'excavator3B':
+        return excavatorMultispriteB;
+      case 'excavator1C':
+      case 'excavator2C':
+      case 'excavator3C':
+        return excavatorMultispriteC;
+      case 'excavator1D':
+      case 'excavator2D':
+      case 'excavator3D':
+        return excavatorMultispriteD;
       case 'conveyor':
         return conveyorTexture;
       default:
